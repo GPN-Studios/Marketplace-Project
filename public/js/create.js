f6ed7b0f4d184c88040306f3d542e9c292df576e
Renan Balter <renan_balter@hotmail.com>
2026-02-15 00:14:37 -0300

    feat/finished js for product price in product creation UI

---
+++ public/js/create.js
@@ -0,0 +1,16 @@
+const input = document.querySelector('#priceInput');
+const hidden = document.querySelector('#priceHidden');
+
+input.addEventListener('input', (e) => {
+    let value = e.target.value.replace(/\D/g, '');
+    if (value.length > 12) value = value.slice(0, 12);
+
+    hidden.value = value;
+
+
+    let number = (value ? Number(value) : 0) / 100;
+    e.target.value = number.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+
+    console.log(value);
+    
+});
